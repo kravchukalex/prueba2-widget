15df2f8a215bb5c876f0a9f5d003c268f909a1ad
FiamaTalavera <fiama.viccini@gmail.com>
2023-12-28 14:44:11 -0300

    testing

--- prueba2-widget.js
+++ prueba2-widget.js
@@ -1,7 +1,7 @@
 ["https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"].forEach((u) => {
   var script = document.createElement("script");
   script.src = u;
-  document.body.appendChild(script);
+  document.head.appendChild(script);
 });
 
 class Prueba2Widget extends HTMLElement {
